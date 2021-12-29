9fab5c3186a08ddaac35678c3afe3a13ce4c68a7
Sandro Matter <github@sandromatter.ch>
2021-12-29 17:25:55 +0100

    Add search and fix results

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,3 +6,36 @@ const swiper = new Swiper(".swiper", {
     enabled: true,
   },
 });
+
+// Jekyll Search
+SimpleJekyllSearch({
+  searchInput: document.getElementById("search-input"),
+  resultsContainer: document.getElementById("results-container"),
+  json: "/search.json",
+  searchResultTemplate: '<li><a href="{url}" title="{desc}">{title}</a></li>',
+  noResultsText: "<p>Hoppla, kein Treffer.</p>",
+  limit: 50,
+  fuzzy: false,
+  exclude: ["Welcome"],
+});
+
+// Check if the results have an input and display it
+let searchInput = document.getElementById("search-input");
+let resultsContainer = document.getElementById("results-container");
+
+searchInput.onkeyup = (elem) => {
+  let userInput = elem.target.value;
+
+  let hide = function (elem) {
+    elem.style.display = "none";
+  };
+  let show = function (elem) {
+    elem.style.display = "block";
+  };
+
+  if (userInput) {
+    show(resultsContainer);
+  } else {
+    hide(resultsContainer);
+  }
+};
