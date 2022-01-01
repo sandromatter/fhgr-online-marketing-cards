a667c26b3a2980b6307c3e64b597c9f046184d83
Sandro Matter <github@sandromatter.ch>
2022-01-01 12:55:03 +0100

    Fix relative URLs for gh-pages

--- src/assets/js/script.js
+++ src/assets/js/script.js
@@ -12,9 +12,9 @@ if (typeof SimpleJekyllSearch === "function") {
   SimpleJekyllSearch({
     searchInput: document.getElementById("search-input"),
     resultsContainer: document.getElementById("results-container"),
-    json: "/search.json",
+    json: "search.json",
     searchResultTemplate:
-      '<li><a tabindex="0" href="{url}" title="{desc}">{title}</a></li>',
+      '<li><a tabindex="0" href="{{ site.baseurl }}/{url}" title="{desc}">{title}</a></li>',
     noResultsText: "<p>Hoppla, kein Treffer.</p>",
     limit: 50,
     fuzzy: false,
